686e273721d8beee7b0665fe158e014766ae4287
Apricitine <apricitinecas@gmail.com>
2023-10-21 16:04:14 -0700

    added deadzones and fixed stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/Methods.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/Methods.java
@@ -8,15 +8,30 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
 public class Methods {
     public static class teleOp {
         public static void robotDrive(double drivePower, HardwareDrive argRobot, Gamepad gamepad, Telemetry telemetry){
-            double directionX = -Math.pow(gamepad.left_stick_x, 1);
-            double directionY = Math.pow(gamepad.left_stick_y, 1);
-            double directionR = -Math.pow(gamepad.right_stick_x, 1);
+            double directionX = 0;
+            double directionY = 0;
+            double directionR = 0;
+
+            if (Math.abs(gamepad.left_stick_x) > 0.2) directionX = Math.pow(gamepad.left_stick_x, 1);
+            if (Math.abs(gamepad.left_stick_y) > 0.2) directionY = -Math.pow(gamepad.left_stick_y, 1);
+            if (Math.abs(gamepad.right_stick_x) > 0.2) directionR = -Math.pow(gamepad.right_stick_x, 1);
 
             double lfPower = (directionX + directionY + directionR) * drivePower;
             double lbPower = (-directionX + directionY + directionR) * drivePower;
             double rfPower = (-directionX + directionY - directionR) * drivePower;
             double rbPower = (directionX + directionY - directionR) * drivePower;
             List<Double> args = List.of(lfPower, lbPower, rfPower, rbPower);
+
+            double max = Math.max(Math.abs(lfPower), Math.abs(rfPower));
+            max = Math.max(max, Math.abs(lbPower));
+            max = Math.max(max, Math.abs(rbPower));
+
+            if (max > 1.0) {
+                lfPower /= max;
+                lbPower /= max;
+                rfPower /= max;
+                rbPower /= max;
+            }
 
             int i = 0;
             for (DcMotorEx chain : List.of(argRobot.lf, argRobot.lb, argRobot.rf, argRobot.rb)) {
