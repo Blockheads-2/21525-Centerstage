2c5d9efb10627efb56814a329e06add953235c7d
Apricitine <apricitinecas@gmail.com>
2023-11-08 15:41:02 -0800

    GOOOAFJADFSKLFJASDLKFJSAL

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/Methods.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/Methods.java
@@ -113,26 +113,39 @@ public class Methods {
                 chain.setPower(arg);
                 i1++;
             }
+        }
+
+        public double driveTrainSpeed() {
+            double drivePower = Constants.DEFAULT_SPEED; //0.75
+            if (gamepad1.right_bumper) drivePower = 1;
+            else if (gamepad1.right_trigger >= 0.2) drivePower = 0.25;
+
+            return drivePower;
+        }
+        public void robotBaseIntakeLoop(HardwareDrive robot) {
+            if (gamepad1.left_bumper) robot.intake.setPower(1);
+            else robot.intake.setPower(0);
+        }
+
+        public void composeTelemetry(double drivePowerAlreadyPassedIntoRobotBaseDriveLoop) {
+            double directionX = 0;
+            double directionY = 0;
+            double directionR = 0;
+
+            if (Math.abs(gamepad1.left_stick_x) > 0.2) directionX = Math.pow(gamepad1.left_stick_x, 1);
+            if (Math.abs(gamepad1.left_stick_y) > 0.2) directionY = -Math.pow(gamepad1.left_stick_y, 1);
+            if (Math.abs(gamepad1.right_stick_x) > 0.2) directionR = -Math.pow(gamepad1.right_stick_x, 1);
+
+            double lfPower = (directionX + directionY + directionR) * drivePowerAlreadyPassedIntoRobotBaseDriveLoop;
+            double lbPower = (-directionX + directionY + directionR) * drivePowerAlreadyPassedIntoRobotBaseDriveLoop;
+            double rfPower = (-directionX + directionY - directionR) * drivePowerAlreadyPassedIntoRobotBaseDriveLoop;
+            double rbPower = (directionX + directionY - directionR) * drivePowerAlreadyPassedIntoRobotBaseDriveLoop;
 
             telemetry.addData("lf power:", lfPower);
             telemetry.addData("lb power:", lbPower);
             telemetry.addData("rf power:", rfPower);
             telemetry.addData("rb power:", rbPower);
-            telemetry.update();
-        }
-        public double driveTrainSpeed(){
-            double drivePower = Constants.DEFAULT_SPEED; //0.75
-            if (gamepad1.right_bumper) drivePower = 1;
-            else if (gamepad1.left_bumper) drivePower = 0.25;
-
-            return drivePower;
-        }
-        public void robotBaseIntakeLoop(HardwareDrive robot) {
-            double intakeVroom;
-
-            if (gamepad1.left_bumper) {
-                robot.intake.setPower(0.25);
-            }
+            telemetry.addData("left bumper pressed:", gamepad1.left_bumper);
         }
     }
 }
