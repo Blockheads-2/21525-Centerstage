5e7890dd4c00e4e651735fbb100ed3ec531bd589
Apricitine <apricitinecas@gmail.com>
2023-11-10 13:00:39 -0800

    add dpad adjustment

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/Methods.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/Methods.java
@@ -15,6 +15,7 @@ import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.sun.tools.javac.util.List;
+
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.teamcode.auto.math.MathConstHead;
@@ -33,11 +34,12 @@ public class Methods {
     public abstract static class auto extends LinearOpMode {
         /**
          * Drive to a position with a specified speed.
+         *
          * @param drivePower value between 0 and 1. Default it to 0.5.
-         * @param finalX final x position of the robot relative to where it was before the method (inches)
-         * @param finalY final y position of the robot relative to where it was before the method (inches)
+         * @param finalX     final x position of the robot relative to where it was before the method (inches)
+         * @param finalY     final y position of the robot relative to where it was before the method (inches)
          */
-        public void updateTelemetry(HardwareDrive robot, TelemetryPacket packet, FtcDashboard dashboard){
+        public void updateTelemetry(HardwareDrive robot, TelemetryPacket packet, FtcDashboard dashboard) {
             packet.put("Top Left Power", robot.lf.getPower());
             packet.put("Top Right Power", robot.rf.getPower());
             packet.put("Bottom Left Power", robot.lb.getPower());
@@ -50,6 +52,7 @@ public class Methods {
 
             dashboard.sendTelemetryPacket(packet);
         }
+
         public void robotAutoStraightDrivePosition(
                 double drivePower,
                 double finalX,
@@ -66,10 +69,10 @@ public class Methods {
             double positiveAngularChangePosition = Math.cos(angleToTarget) + Math.sin(angleToTarget);
             double negativeAngularChangePosition = Math.cos(angleToTarget) - Math.sin(angleToTarget);
 
-            robot.lf.setTargetPosition((int)(robot.lf.getCurrentPosition() + (positiveAngularChangePosition * CPI * distanceToTarget)));
-            robot.lb.setTargetPosition((int)(robot.lb.getCurrentPosition() + (negativeAngularChangePosition * CPI * distanceToTarget)));
-            robot.rf.setTargetPosition((int)(robot.rf.getCurrentPosition() + (negativeAngularChangePosition * CPI * distanceToTarget)));
-            robot.rb.setTargetPosition((int)(robot.rb.getCurrentPosition() + (positiveAngularChangePosition * CPI * distanceToTarget)));
+            robot.lf.setTargetPosition((int) (robot.lf.getCurrentPosition() + (positiveAngularChangePosition * CPI * distanceToTarget)));
+            robot.lb.setTargetPosition((int) (robot.lb.getCurrentPosition() + (negativeAngularChangePosition * CPI * distanceToTarget)));
+            robot.rf.setTargetPosition((int) (robot.rf.getCurrentPosition() + (negativeAngularChangePosition * CPI * distanceToTarget)));
+            robot.rb.setTargetPosition((int) (robot.rb.getCurrentPosition() + (positiveAngularChangePosition * CPI * distanceToTarget)));
 
             for (DcMotorEx motor : List.of(robot.lf, robot.lb, robot.rf, robot.rb)) {
                 motor.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
@@ -105,7 +108,7 @@ public class Methods {
         protected FtcDashboard dashboard;
         protected TelemetryPacket packet;
 
-        public void init_robot(){
+        public void init_robot() {
             robot.init(hardwareMap);
             /*robot.initCamera();*/
 
@@ -121,16 +124,19 @@ public class Methods {
             packet = new TelemetryPacket();
             dashboard.setTelemetryTransmissionInterval(25);
         }
-        
-        public void robotBaseDriveLoop(double drivePower){
+
+        public void robotBaseDriveLoop(double drivePower) {
             double directionX = 0;
             double directionY = 0;
             double directionR = 0;
             int i1 = 0;
 
-            if (Math.abs(gamepad1.left_stick_x) > 0.2) directionX = Math.pow(gamepad1.left_stick_x, 1);
-            if (Math.abs(gamepad1.left_stick_y) > 0.2) directionY = -Math.pow(gamepad1.left_stick_y, 1);
-            if (Math.abs(gamepad1.right_stick_x) > 0.2) directionR = -Math.pow(gamepad1.right_stick_x, 1);
+            if (Math.abs(gamepad1.left_stick_x) > 0.2)
+                directionX = Math.pow(gamepad1.left_stick_x, 1);
+            if (Math.abs(gamepad1.left_stick_y) > 0.2)
+                directionY = -Math.pow(gamepad1.left_stick_y, 1);
+            if (Math.abs(gamepad1.right_stick_x) > 0.2)
+                directionR = -Math.pow(gamepad1.right_stick_x, 1);
 
             double lfPower = (directionX + directionY + directionR) * drivePower;
             double lbPower = (-directionX + directionY + directionR) * drivePower;
@@ -158,23 +164,43 @@ public class Methods {
             telemetry.addData("rb power:", rbPower);
             telemetry.update();
         }
-        public double driveTrainSpeed(){
+
+        public double driveTrainSpeed() {
             double drivePower = Constants.DEFAULT_SPEED; //0.75
             if (gamepad1.right_bumper) drivePower = 1;
-            else if (gamepad1.left_bumper) drivePower = 0.25;
+            else if (gamepad1.right_trigger >= 0.1) drivePower = 0.25;
 
             return drivePower;
         }
+
         public void robotBaseIntakeLoop() {
-            double intakeVroom;
-//
-//            if (gamepad1.left_bumper) {
-//                robot.intake.setPower(0.25);
-//            }
             robot.intake.setPower(gamepad1.left_trigger);
         }
 
-        public void dpad(){}
+        public void robotBaseMicroAdjustLoop(double drivePower) {
+            if (gamepad1.dpad_up) {
+                robot.lf.setPower(-0.4);
+                robot.lb.setPower(-0.4);
+                robot.rf.setPower(+0.4);
+                robot.rb.setPower(+0.4);
+            } else if (gamepad1.dpad_down) {
+                robot.lf.setPower(+0.4);
+                robot.lb.setPower(+0.4);
+                robot.rf.setPower(-0.4);
+                robot.rb.setPower(-0.4);
+            } else if (gamepad1.dpad_right) {
+                robot.lf.setPower(0.4);
+                robot.lb.setPower(0.4);
+                robot.rf.setPower(0.4);
+                robot.rb.setPower(0.4);
+            } else if (gamepad1.dpad_left) {
+                robot.lf.setPower(-0.4);
+                robot.lb.setPower(-0.4);
+                robot.rf.setPower(-0.4);
+                robot.rb.setPower(-0.4);
+            }
+
+        }
 
     }
 }
